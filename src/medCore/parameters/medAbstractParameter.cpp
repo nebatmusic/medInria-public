e88e6780b1c0b0bd38ef9315e0e7a7a08b989139
nicolas schnitzler <nicolas.schnitzler@inria.fr>
2015-03-02 13:07:55 +0100

    resolving conflicts
    Conflicts:
    	src-plugins/itkDataImage/interactors/medVtkViewItkDataImageInteractor.cpp
    	src/medCore/parameters/medAbstractParameter.cpp

--- src/medCore/parameters/medAbstractParameter.cpp
+++ src/medCore/parameters/medAbstractParameter.cpp
@@ -123,9 +123,9 @@ void medAbstractParameter::blockInternWidgetsSignals(bool block) const
 
 void medAbstractParameter::toXMLNode(QDomDocument* doc,QDomElement* currentNode)
 {
-    QDomElement elmt=doc->createElement("name");
-    elmt.appendChild(doc->createTextNode(name()));
-    currentNode->appendChild(elmt);
+	QDomElement elmt=doc->createElement("name");
+	elmt.appendChild(doc->createTextNode(name()));
+	currentNode->appendChild(elmt);
 }
 
 //--------------------------------------------------------------------------
@@ -321,20 +321,9 @@ void medAbstractVector2DParameter::setValue(const QVector2D &value)
 
     emit valueChanged(value);
 }
-QVector2D medAbstractVector2DParameter::value() const
-{
-    return m_value;
-}
-
 void medAbstractVector2DParameter::trigger()
 {
     emit valueChanged(m_value);
-}
-
-QVector2D medAbstractVector2DParameter::fromString(QString value)
-{
-    QStringList tokens=value.split(" ");
-    return QVector2D(tokens[0].toDouble(),tokens[1].toDouble());
 }
 
 void medAbstractVector2DParameter::toXMLNode(QDomDocument* doc,QDomElement* currentNode)
@@ -347,6 +336,18 @@ void medAbstractVector2DParameter::toXMLNode(QDomDocument* doc,QDomElement* curr
     elmt.appendChild(doc->createTextNode(QString::number(value().x())+" "+QString::number(value().y())));
     currentNode->appendChild(elmt);
 }
+
+QVector2D medAbstractVector2DParameter::fromString(QString value)
+{
+    QStringList tokens=value.split(" ");
+    return QVector2D(tokens[0].toDouble(),tokens[1].toDouble());
+}
+
+QVector2D medAbstractVector2DParameter::value() const
+{
+    return m_value;
+}
+
 //--------------------------------------------------------------------------
 //  medAbstractVector3DParameter
 void medAbstractVector3DParameter::setValue(const QVector3D &value)
