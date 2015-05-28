49c8e41dfb682a3372cdfe002c28f756134fb96f
mathilde.merle <mathilde.merle@ihu-liryc.fr>
2015-05-28 11:52:38 +0200

    [Theme] logo-txt-authors-license
    Conflicts:
    	LICENSE.txt

--- app/medInria/medApplication.cpp
+++ app/medInria/medApplication.cpp
@@ -79,7 +79,7 @@ medApplication::medApplication(int & argc, char**argv) :
     this->setApplicationName("MUSIC Viewer");
     qDebug() << "Version:" << MEDINRIA_VERSION;
     this->setApplicationVersion(MEDINRIA_VERSION);
-    this->setOrganizationName("inria");
+    this->setOrganizationName("INRIA & IHU LIRYC");
     this->setOrganizationDomain("fr");
     this->setWindowIcon(QIcon(":music_logo_small.png"));
 
